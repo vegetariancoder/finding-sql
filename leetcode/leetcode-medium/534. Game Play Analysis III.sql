3a7f2fb199eb48a8fe2225967814c28abe0a1669
sahilnagpal <thisissahilnagpal@gmail.com>
2023-01-30 23:16:55 -0500

    Monday code

---
+++ leetcode/leetcode-medium/534. Game Play Analysis III.sql
@@ -0,0 +1,21 @@
+use leetcode;
+
+CREATE TABLE IF NOT EXISTS Activity (player_id int, device_id int, event_date date, games_played int);
+insert into Activity (player_id, device_id,event_date,games_played) values (1,2,'2016-03-01',5);
+insert into Activity (player_id, device_id,event_date,games_played) values (1,2,'2016-05-02',6);
+insert into Activity (player_id, device_id,event_date,games_played) values (1,3,'2017-06-25',1);
+insert into Activity (player_id, device_id,event_date,games_played) values (3,1,'2016-03-02',0);
+insert into Activity (player_id, device_id,event_date,games_played) values (3,4,'2018-07-03',5);
+
+
+with answer as (select
+    *,
+    sum(games_played) over (partition by player_id order by event_date) as games_played_so_far
+from
+    Activity)
+select
+    player_id,
+    event_date,
+    games_played_so_far
+from
+    answer;
