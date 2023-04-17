7b7f86089036190e553136bae62643b71e80130e
sahilnagpal <thisissahilnagpal@gmail.com>
2023-04-17 09:49:15 -0400

    Learning

---
+++ leetcode/leetcode-medium/2346. Compute the Rank as a Percentage.sql
@@ -0,0 +1,21 @@
+use leetcode;
+CREATE TABLE IF NOT EXISTS Students (student_id int, department_id  int, mark int);
+insert into Students (student_id, department_id, mark) values (2,2,650);
+insert into Students (student_id, department_id, mark) values (8,2,650);
+insert into Students (student_id, department_id, mark) values (7,1,920);
+insert into Students (student_id, department_id, mark) values (1,1,610);
+insert into Students (student_id, department_id, mark) values (3,1,530);
+
+with answer as (
+select
+    *,
+    dense_rank() over (partition by department_id order by mark desc ) as drnk,
+    count(student_id) over (partition by department_id) as cnt
+from
+    Students)
+select
+    student_id,
+    department_id,
+    round((drnk-1) * 100 / (cnt-1),2) as percentage
+from
+    answer;
